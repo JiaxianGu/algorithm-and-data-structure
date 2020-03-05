a8261984a396744c70a4384486d20dcec56a0c40
Jiaxian Gu <gujiaxian1997@gmail.com>
2020-03-05 15:31:33 +0900

    20200305

---
+++ 26/26.py
@@ -0,0 +1,14 @@
+def removeDuplicates(nums):
+    # length = 0
+    # previous = None
+    # for i in nums:
+    #     if i == previous:
+    #         continue
+    #     else:
+    #         previous = i
+    #         length += 1
+    # return length
+    
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+print(removeDuplicates(nums))
